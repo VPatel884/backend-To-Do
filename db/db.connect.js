8b0d2ee91dbb296546d07b77c7f9c94db69584ec
Vikash Patel <vpatel884@gmail.com>
2025-05-13 14:37:09 +0530

    feat: Todo model created.

---
+++ db/db.connect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const mongoUri = process.env.MONGODB;
+
+const initializeDatabase = async () => {
+    await mongoose.connect(mongoUri).then(() => {
+    console.log("Connected to the database.")
+}).catch((error) => {
+    console.log("Error connecting to the database", error)
+})
+}
+
+module.exports = {initializeDatabase};
