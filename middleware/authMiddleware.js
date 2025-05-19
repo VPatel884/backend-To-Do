d026a1abbace7636608f92bfa7a5f76d44bf47ca
Vikash Patel <vpatel884@gmail.com>
2025-05-19 09:50:46 +0530

    To check if token already exists

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,18 +4,24 @@ const User = require("../models/User.model");
 const protect = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (authHeader && authHeader.startsWith("Bearer")) {
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+
+    if (!token || token === 'null') {
+      return res.status(401).json({ message: "No token, authorization denied" });
+    }
+
     try {
-      const token = authHeader.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
       next();
     } catch (err) {
-      res.status(401).json({ message: "Not authorized, token failed" });
+      return res.status(401).json({ message: "Not authorized, token failed" });
     }
   } else {
-    res.status(401).json({ message: "No token, authorization denied" });
+    return res.status(401).json({ message: "No token, authorization denied" });
   }
 };
+
 
 module.exports = { protect };
