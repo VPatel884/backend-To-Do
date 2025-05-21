a94361a67fbbe931f0aa9ff0735a69c9fd6b6efb
Vikash Patel <vpatel884@gmail.com>
2025-05-21 09:27:34 +0530

    feat: Configure CORS to allow all origins

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 initializeDatabase();
 
